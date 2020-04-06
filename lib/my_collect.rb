175789b50936fb70adc42abcd5138a2735aee1d0
Adam Spencer <aspencer0919@gmail.com>
2020-04-06 12:21:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,10 @@ collection = []
 def my_collect(collection)
   i = 0
   result = []
-  while i < collection
+  while i < collection.length
     result << yield(collection)
     i += 1
   end
   result
 end
+
